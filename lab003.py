4d1d050e4bd3b8aab0e934b19cd3dc6ca411b5b2
0ddbox <oddi95k@gmail.com>
2024-09-10 13:46:33 -0700

    Add files via upload

---
+++ lab003.py
@@ -0,0 +1,49 @@
+# libraries
+import lab_chat as lc
+
+
+# functions
+def get_username():
+    username = input("Enter your username: ")
+    cleaned_username = username.strip().upper()
+    return cleaned_username
+
+
+def get_group():
+    group = input("Group name: ")
+    cleaned_group = group.strip().upper()
+    return cleaned_group
+
+
+def get_message():
+    message = input("Message: \n")
+    cleaned_message = message.strip().upper()
+    return cleaned_message
+
+
+def init_chat():
+    username = get_username()
+    group_name = get_group()
+
+    node = lc.get_peer_node(username)
+    lc.join_group(node, group_name)
+
+    chat_channel = lc.get_channel(node, group_name)
+
+    return chat_channel
+
+
+def start_chat():
+    channel = init_chat()
+
+    while True:
+        try:
+            msg = get_message()
+            channel.send(msg.encode('utf_8'))
+        except (KeyboardInterrupt, SystemExit):
+            break
+    channel.send("$$STOP".encode('utf_8'))
+    print("FINISHED")
+
+
+start_chat()
